1c4f77ce971e6bc4dbcc57ad6395c0a895be5184
silkytiger <triantafyllides@gmail.com>
2018-01-17 10:37:10 -0500

    WalkBetter: attatch to camera rig, defaults to left wand. BoundaryEnforcer: completely broken, do not use

---
+++ BoundaryEnforcer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryEnforcer : MonoBehaviour
+{
+    float gravity = (float)-9.8;
+
+    public Transform cameraRigTransform;
+    bool colliding;
+
+    Vector3 v_0;
+    DateTime t_0;
+    float height_at_impact;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        colliding = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        colliding = true;
+        height_at_impact = cameraRigTransform.transform.position.y;
+
+        v_0 = -collision.relativeVelocity;
+        t_0 = DateTime.Now;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        colliding = false;
+    }
+
+    void Update()
+    {
+        float dt = (float) new TimeSpan(DateTime.Now.Ticks - t_0.Ticks).TotalSeconds;
+
+        v_0.y += gravity * dt;
+        t_0 = DateTime.Now;
+
+        //translate camera rig by change in velocity over time
+        cameraRigTransform.transform.position += v_0 * dt;
+
+        //if we have hit the bottom, we stop moving
+        if (cameraRigTransform.transform.position.y <= height_at_impact)
+        {
+            v_0 = Vector3.zero;
+            //cameraRigTransform.transform = height_at_impact;
+        }
+    }
+}
---
+++ WalkBetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkBetter : MonoBehaviour
+{
+    private SteamVR_Controller.Device LeftController;
+
+    void Awake()
+    {
+        LeftController = SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.FarthestLeft));
+    }
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 touchpadVector = LeftController.GetAxis();
+        touchpadVector.Normalize();
+        touchpadVector /= 50;
+
+        float controllerRotation = -1 * LeftController.transform.rot.y * Mathf.PI;
+
+        float x = touchpadVector.x * Mathf.Cos(controllerRotation) - touchpadVector.y * Mathf.Sin(controllerRotation);
+        float z = touchpadVector.x * Mathf.Sin(controllerRotation) + touchpadVector.y * Mathf.Cos(controllerRotation);
+
+        gameObject.transform.position += new Vector3(x, 0, z);
+    }
+}
